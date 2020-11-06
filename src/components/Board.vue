84f9030fce19254a44dab253093d8f01b861a3ff
Ingvar <ingvar@tmd.ee>
2020-11-06 21:26:24 +0200

    centered gameboard

---
+++ src/components/Board.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="board">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.board {
+    width: 100vw;
+    margin-top: 5vh;
+    height: 90vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-image: url("../assets/Gameboard.png");
+}
+</style>
