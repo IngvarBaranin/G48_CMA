b292da4ddf42584c6ba4f598cc22c96cec3e66ea
rebekkabb <breedis.rebekka@gmail.com>
2020-11-07 04:32:57 +0200

    Added simple error-handling server side for unknown lobbies

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,19 +30,42 @@ server.put("/lobby", (req, res) => {
 
 // Joining a lobby
 server.post("/lobby", (req, res) => {
-    let lobby = lobbies[req.body.id];
-    joinLobby(req.body.id, req.body.user);
+    try {
+        let lobby = getLobby(req.body.id, res);
+        joinLobby(req.body.id, req.body.user);
 
-    console.log(`${JSON.stringify(lobby)} Created lobby ${JSON.stringify(lobby)}`);
+        console.log(`${JSON.stringify(lobby)} Created lobby ${JSON.stringify(lobby)}`);
 
-    res.send(lobby);
+        res.send(lobby);
+    } catch (e) {
+        res.status(422).send(e);
+    }
 });
 
 // Return lobby info
 server.get("/lobby/:id", (req, res) => {
     console.log(`Get lobby ${req.params.id} info`);
-    res.send(lobbies[req.params.id]);
+    try {
+        const lobby = getLobby(req.params.id, res);
+        res.send(lobby);
+    } catch (e) {
+        res.status(422).send(e);
+    }
 });
+
+/**
+ * Get lobby or throw error if lobby does not exist
+ * @param lobbyId
+ * @param res
+ */
+function getLobby(lobbyId, res) {
+    const lobby = lobbies[lobbyId];
+    if (lobby === undefined) {
+        throw {error: "Could not find lobby"};
+    } else {
+        return lobby;
+    }
+}
 
 function createLobby() {
     return {
@@ -56,10 +79,10 @@ function joinLobby(lobbyId, user) {
 }
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
